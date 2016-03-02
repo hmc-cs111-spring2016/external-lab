4597b526d6e458c9228f12f7d2ffbb49fe5c6fdb
amitmaor <amitmaor@WL-197-78.WPA.Claremont.Edu>
2016-03-02 15:57:52 -0800

    hooray done

--- src/main/scala/calculator/ir/AST.scala
+++ src/main/scala/calculator/ir/AST.scala
@@ -4,3 +4,7 @@ sealed abstract class Expr
 
 case class Num(n: Int) extends Expr
 case class Plus(left: Expr, right: Expr) extends Expr
+case class Minus(left:Expr, right: Expr) extends Expr
+case class Times(left:Expr, right: Expr) extends Expr
+case class Divide(left:Expr, right: Expr) extends Expr
+case class Parens(expr:Expr) extends Expr
--- src/main/scala/calculator/parser/Parser.scala
+++ src/main/scala/calculator/parser/Parser.scala
@@ -23,12 +23,20 @@ object CalcParser extends JavaTokenParsers with PackratParsers {
 
     // expressions
     lazy val expr: PackratParser[Expr] = 
-      (   expr~"+"~fact ^^ {case l~"+"~r ⇒ Plus(l, r)}
+      (   expr~"+"~term ^^ {case l~"+"~r ⇒ Plus(l, r)}
+        | expr~"-"~term ^^ {case l~"-"~r ⇒ Minus(l, r)}
+        | term )
+    
+    // term
+    lazy val term: PackratParser[Expr] = 
+      (   term~"*"~fact ^^ {case l~"*"~r ⇒ Times(l, r)}
+        | term~"/"~fact ^^ {case l~"/"~r ⇒ Divide(l, r)}
         | fact )
         
     // factors
     lazy val fact: PackratParser[Expr] =
-      number
+      (   "("~expr~")" ^^ {case "("~e~")" ⇒ Parens(e)}
+        | number )
       
     // numbers
     def number: Parser[Num] = wholeNumber ^^ {s ⇒ Num(s.toInt)}
