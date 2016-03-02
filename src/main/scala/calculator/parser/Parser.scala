3cec7808d4aee413073fb0a9e66cec706c920ad2
williumchen <wichen@g.hmc.edu>
2016-03-02 15:55:43 -0800

    finished +,-,*,/,()

--- src/main/scala/calculator/parser/Parser.scala
+++ src/main/scala/calculator/parser/Parser.scala
@@ -23,14 +23,21 @@ object CalcParser extends JavaTokenParsers with PackratParsers {
 
     // expressions
     lazy val expr: PackratParser[Expr] = 
-      (   expr~"+"~fact ^^ {case l~"+"~r ⇒ Plus(l, r)}
-        | fact )
+      (   expr~"+"~term ^^ {case l~"+"~r ⇒ Plus(l, r)}
+        | expr~"-"~term ^^ {case l~"-"~r => Minus(l, r)}
+        | term )
+    
+    // term
+    lazy val term: PackratParser[Expr] = 
+      (   expr~"*"~fact ^^ {case l~"*"~r => Mult(l, r)}
+      | expr~"/"~fact ^^ {case l~"/"~r => Div(l,r)}
+      | fact)
         
     // factors
     lazy val fact: PackratParser[Expr] =
-      number
+//      number | "("~>expr<~")"
+    number | "("~expr~")" ^^ {case "("~expr~")" => Paren(expr)}
       
     // numbers
     def number: Parser[Num] = wholeNumber ^^ {s ⇒ Num(s.toInt)}
-    
  }
